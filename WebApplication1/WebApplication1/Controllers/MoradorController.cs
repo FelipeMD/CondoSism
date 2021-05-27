b732858f67320244cf4e4466058c7a2a883462a3
Felipe Souza <felipe.macieldsouza@gmail.com>
2021-05-27 20:53:46 -0300

    criando as classes de dominio e o ValueObjetc de Cpf

---
+++ WebApplication1/WebApplication1/Controllers/MoradorController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+        
+    }
+}
---
+++ WebApplication1/WebApplication1/Domain/Morador.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using WebApplication1.Domain.ValueObjetcs;
+
+namespace WebApplication1.Domain
+{
+    public class Morador
+    {
+        public long Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateFormat DataNasciment { get; set; }
+        public long Telefone { get; set; }
+        public Cpf Cpf { get; set; }
+        public string Email { get; set; }
+    }
+}
