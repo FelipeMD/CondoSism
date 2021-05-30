df3ab8da791cd72ed5d2b2840bcd27aa06617834
Felipe Souza <felipe.macieldsouza@gmail.com>
2021-05-30 01:32:34 -0300

    bugfix-bug-autenticacao-corrigido

---
+++ WebApplication1/WebApplication1/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data.ValueObjetcs;
+using WebApplication1.Domain.Login.Interfaces;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private ILoginService _loginService;
+
+        public AuthController(ILoginService loginService)
+        {
+            _loginService = loginService;
+        }
+
+        [HttpPost]
+        [Route("signin")]
+        public IActionResult Signin([FromBody] UserVo user)
+        {
+            if (user == null) return BadRequest("Invalid client request");
+            var token = _loginService.ValidateCredentials(user);
+            if (token == null) return Unauthorized();
+            return Ok(token);
+        }
+    }
+}
