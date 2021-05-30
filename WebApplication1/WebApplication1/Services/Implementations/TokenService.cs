93976ce3686cdc119106a88a5ee0abcede32aba2
Felipe Souza <felipe.macieldsouza@gmail.com>
2021-05-30 00:35:22 -0300

    implementando mais dois metodos em TokenService

--- WebApplication1/WebApplication1/Services/Implementations/TokenService.cs
+++ WebApplication1/WebApplication1/Services/Implementations/TokenService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
 using WebApplication1.Configurations;
@@ -35,12 +36,32 @@ namespace WebApplication1.Services.Implementations
 
         public string GenerateRefreshToken()
         {
-            throw new System.NotImplementedException();
+            var randomNumber = new byte[32];
+            using (var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomNumber);
+                return Convert.ToBase64String(randomNumber);
+            }
         }
 
         public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
         {
-            throw new System.NotImplementedException();
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.Secret)),
+                ValidateLifetime = false
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            ClaimsPrincipal principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+            var jwtSecurityToken = securityToken as JwtSecurityToken;
+            if (jwtSecurityToken == null ||
+                !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCulture)) 
+                throw new SecurityTokenException("Invalid Token");
+            
+            return principal;
         }
     }
 }
