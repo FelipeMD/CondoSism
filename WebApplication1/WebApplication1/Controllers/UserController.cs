c85dc18fb35c6257fc5edcd1948450e36f85f30a
Felipe Souza <felipe.macieldsouza@gmail.com>
2021-05-29 23:15:07 -0300

    add dependencias

---
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Components.Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        
+    }
+}
