81e000edc2bb8c10d7b274cbc72ec69463331df9
FelipeMD <felipe.macieldsouza@gmail.com>
2021-10-08 20:15:20 -0300

    adicionando docker ao projeto

--- WebApplication1/WebApplication1/obj/Release/net5.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Release/net5.0/WebApplication1.AssemblyInfo.cs
@@ -11,9 +11,8 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2deb51e9-c89d-4304-b352-270ae60c6cdf")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
